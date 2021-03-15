7eb4291f16e18fd8c091f4881af3e803ab143acd
hernanpm101 <hernanpm101@hotmail.com>
2021-03-15 18:41:40 -0300

    homework1.1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -220,6 +220,7 @@ function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
   return lado * 4
+
 }
 
 
